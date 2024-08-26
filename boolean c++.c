225014260aa2f8620ef1a2c781ca10087c6e32b6
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:24:01 +0530

    Create boolean c++.c

---
+++ boolean c++.c
@@ -0,0 +1,12 @@
+
+// variable created with the bool keyword that can only take the value of true and false
+#include <iostream>
+using namespace std;
+int main(){
+    bool iscodingFun = "true";
+    bool isfishTasty = "false";
+    cout<< iscodingFun << "\n";
+    cout<< isfishTasty << "\n";
+    return 0;
+
+}
